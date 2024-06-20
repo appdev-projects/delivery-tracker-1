173743cba8af4f0d9837032b1ec99428dfbf85be
AdolfoNava <adolfonava1990@gmail.com>
2024-06-20 17:03:32 -0500

    Finished Assignment

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,9 +1,9 @@
 class DeliveriesController < ApplicationController
   def index
-    matching_deliveries = Delivery.all
+    matching_deliveries = Delivery.where(:user_id => current_user.id, :arrived => false)
 
-    @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
-
+    @waiting_deliveries = matching_deliveries.order({ :created_at => :desc })
+    @arrived_deliveries = Delivery.where(:user_id => current_user.id, :arrived => true).order({:created_at => :desc})
     render({ :template => "deliveries/index" })
   end
 
@@ -19,15 +19,15 @@ class DeliveriesController < ApplicationController
 
   def create
     the_delivery = Delivery.new
-    the_delivery.user_id = params.fetch("query_user_id")
+    the_delivery.user_id = params["query_user_id"]
     the_delivery.details = params.fetch("query_details")
     the_delivery.description = params.fetch("query_description")
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.arrived = params.fetch("query_arrived", false)
-
+    the_delivery.arrived = false
+    
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries", { :notice => "Delivery created successfully." })
+      redirect_to("/deliveries/", { :notice => "Added to list" })
     else
       redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
@@ -37,11 +37,7 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.user_id = params.fetch("query_user_id")
-    the_delivery.details = params.fetch("query_details")
-    the_delivery.description = params.fetch("query_description")
-    the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.arrived = params.fetch("query_arrived", false)
+    the_delivery.arrived = true
 
     if the_delivery.valid?
       the_delivery.save
