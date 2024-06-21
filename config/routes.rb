981de1ba3e38b8572b0b4766295d99298f474e81
SaraDawner2000 <83845798+SaraDawner2000@users.noreply.github.com>
2024-06-21 10:51:48 -0500

    change routes to rails CRUD convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "deliveries#index"
   devise_for :users
-  get "/delete_delivery/:id", controller: "deliveries", action: "destroy"
-  post "/modify_delivery/:id", controller: "deliveries", action: "update"
   resources :deliveries
 
 end
