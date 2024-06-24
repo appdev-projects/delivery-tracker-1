a9ac49d02a34a2fecfdd7ed3beb7690a738e0d9e
cindellai <cindellai26@gmail.com>
2024-06-23 20:32:06 -0500

    "Updated packages controller, delivery model, and views to add new fields and modify form submissions; updated database schema to include new fields"

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,10 +2,14 @@
 #
 # Table name: deliveries
 #
-#  id         :integer          not null, primary key
-#  arrived    :boolean          default(FALSE)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                    :integer          not null, primary key
+#  arrived               :boolean          default(FALSE)
+#  description           :string
+#  details               :text
+#  supposed_to_arrive_on :date
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  user_id               :integer
 #
 class Delivery < ApplicationRecord
   belongs_to :user
