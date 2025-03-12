6c621df289433d5a4b7f6013284cf200bb32daa0
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2025-03-12 20:31:46 +0000

    corrige

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many: :deliveries, class_name: "Delivery", foreign_key: "user_id", dependent: :destroy
+  has_many :deliveries, class_name: "Delivery", foreign_key: "user_id", dependent: :destroy
 end
