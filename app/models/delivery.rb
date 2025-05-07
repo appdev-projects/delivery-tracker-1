170acb70d07dd1ce5e58264f1266e6650ce8f199
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-07 22:49:37 +0000

    And I removed "received" because it wasn't doing anything really.

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -6,7 +6,6 @@
 #  arrived               :boolean
 #  description           :string
 #  details               :text
-#  received              :boolean
 #  supposed_to_arrive_on :date
 #  created_at            :datetime         not null
 #  updated_at            :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_07_224639) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_07_224851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_07_224639) do
     t.string "description"
     t.text "details"
     t.date "supposed_to_arrive_on"
-    t.boolean "received"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
