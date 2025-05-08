4f0616fb51a74dbf9f8cf57f2e61daef080d6c40
Sabreena Salomon <salomonbree@gmail.com>
2025-05-08 01:06:24 +0000

    added user_id to create_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_05_235616) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_05_231838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "deliveries", force: :cascade do |t|
+    t.integer "user_id"
     t.string "description"
     t.date "supposed_to_arrive_on"
     t.text "details"
+    t.boolean "arrived"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
