6e5da5a82c07e91dbaf65a33fb546fea31460c85
Victor Manzanilla <67130786+VictorManzanilla@users.noreply.github.com>
2025-05-23 17:41:46 +0000

    working update

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -19,14 +19,14 @@ class DeliveriesController < ApplicationController
 
   def create
     the_delivery = Delivery.new
-    the_delivery.user_id = params.fetch("query_user_id")
+    the_delivery.user_id = current_user.id
     the_delivery.description = params.fetch("query_description")
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
     the_delivery.details = params.fetch("query_details")
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries", { :notice => "Delivery created successfully." })
+      redirect_to("/deliveries", { :notice => "Added to list." })
     else
       redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
@@ -34,18 +34,20 @@ class DeliveriesController < ApplicationController
 
   def update
     the_id = params.fetch("path_id")
-    the_delivery = Delivery.where({ :id => the_id }).at(0)
+    @the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.user_id = params.fetch("query_user_id")
-    the_delivery.description = params.fetch("query_description")
-    the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.details = params.fetch("query_details")
 
-    if the_delivery.valid?
-      the_delivery.save
-      redirect_to("/deliveries/#{the_delivery.id}", { :notice => "Delivery updated successfully."} )
+    # @the_delivery.user_id = current_user.id
+    # @the_delivery.description = params.fetch("query_description")
+    # @the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
+    # @the_delivery.details = params.fetch("query_details")
+
+    if @the_delivery.valid?
+      @the_delivery.save
+      
+      redirect_to("/deliveries", { :notice => "Marked as received."} )
     else
-      redirect_to("/deliveries/#{the_delivery.id}", { :alert => the_delivery.errors.full_messages.to_sentence })
+      redirect_to("/deliveries", { :alert => @the_delivery.errors.full_messages.to_sentence })
     end
   end
 
