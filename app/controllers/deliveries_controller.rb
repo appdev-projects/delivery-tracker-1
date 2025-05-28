c2cec4470ea6eb4fcc93e4fb58197506c52e76c7
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2025-05-28 11:16:17 -0500

    updated controller

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,12 +1,19 @@
 class DeliveriesController < ApplicationController
   def index
-    matching_deliveries = Delivery.all
 
-    @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
+    # if there is no userthen redirects to the login page
+    if current_user == nil
+      redirect_to("/users/sign_in")
 
-    render({ :template => "deliveries/index" })
+    # else if there is user then render the index page of deliveries 
+    else
+      matching_deliveries = Delivery.all
+
+      @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
+
+      render({ :template => "deliveries/index" })
+    end
   end
-
   def show
     the_id = params.fetch("path_id")
 
@@ -37,11 +44,11 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.description = params.fetch("query_description")
-    the_delivery.details = params.fetch("query_details")
-    the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
+    #the_delivery.description = params.fetch("query_description")
+    #the_delivery.details = params.fetch("query_details")
+    #the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
     the_delivery.arrived = params.fetch("query_arrived", false)
-    the_delivery.user_id = params.fetch("query_user_id")
+    #the_delivery.user_id = params.fetch("query_user_id")
 
     if the_delivery.valid?
       the_delivery.save
